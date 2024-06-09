5014287882b71da1cf828bf9e10990f7ad8dd798
JuaniV2002 <juani2002villa@icloud.com>
2024-06-09 16:24:14 -0300

    http delete request

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -73,10 +73,21 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+    
+    final url = Uri.https('shopping-ist-app-default-rtdb.firebaseio.com', 'shopping_list/${item.id}.json');
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+        _error = 'An error occurred while deleting the item!';
+      });
+    }
   }
 
   @override
